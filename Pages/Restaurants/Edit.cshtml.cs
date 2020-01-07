0a1d00fd729f1df49e1fb02b84eb13a0be1af5a5
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2020-01-07 18:20:51 +0100

    Building the Edit use case of the HymnALaBouffe

--- Pages/Restaurants/Edit.cshtml.cs
+++ Pages/Restaurants/Edit.cshtml.cs
@@ -6,21 +6,27 @@ using HymnALaBouffe.Data;
 using HymneALaBouffe.CoreBusiness;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HymneALaBouffe.Pages.Restaurants
 {
     public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
 
         public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
 
-        public EditModel(IRestaurantData restaurantData )
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper )
         {
             this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
         }
         public IActionResult OnGet(int restaurantId)
         {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+
             Restaurant = restaurantData.GetById(restaurantId);
 
             if(Restaurant == null)
